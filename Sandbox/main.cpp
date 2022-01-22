bec49b250505cf46afa1a62f0f4135980b591a51
= <alvar.alonso.2010@gmail.com>
2022-01-22 13:51:40 +0100

    query descriptor sizes and msaa support

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -26,12 +26,43 @@ using Microsoft::WRL::ComPtr;
 
 global_variable bool running = false;
 
-struct Direct3DComponents
+struct D3DFence
+{
+    Microsoft::WRL::ComPtr<ID3D12Fence> handle;
+    uint64_t currentFence = 0;
+};
+
+struct D3DDescriptorSizes
+{
+    uint32_t rtv;
+    uint32_t dsv;
+    uint32_t srv_uav_cbv;
+} dx12DescriptorSizes;
+
+struct D3DComponents
 {
     Microsoft::WRL::ComPtr<IDXGIFactory4> dxgiFactory;
     Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain;
     Microsoft::WRL::ComPtr<ID3D12Device> logicalDevice;
 } dx12Components;
+
+struct WindowImageInfo
+{
+    DXGI_FORMAT backBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+    DXGI_FORMAT depthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    uint32_t clientWidth = 800;
+    uint32_t clientHeight = 600;
+} windowImageInfo;
+
+struct D3DShyncPrimitives
+{
+    D3DFence fence;
+} dx12SynchPrimitives;
+
+struct D3DFeaturesSupport
+{
+    uint32_t msaa4xQuality;
+} dx12FeaturesSupport;
 
 LRESULT CALLBACK
 MainWindowCallback(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
@@ -141,6 +172,36 @@ WinMain(HINSTANCE hInstance,
             D3D_FEATURE_LEVEL_11_0,
             IID_PPV_ARGS(&dx12Components.logicalDevice));
     }
+
+    // synchcronitzation primitives
+    dx12Components.logicalDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE,
+        IID_PPV_ARGS(&dx12SynchPrimitives.fence.handle));
+
+    // descriptor sizes
+    dx12DescriptorSizes.rtv = dx12Components.logicalDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    dx12DescriptorSizes.dsv = dx12Components.logicalDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+    dx12DescriptorSizes.srv_uav_cbv = dx12Components.logicalDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+    // Check 4x MSAA quality support
+    D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msaaQualityLevels;
+    msaaQualityLevels.Format = windowImageInfo.backBufferFormat;
+    msaaQualityLevels.SampleCount = 4;
+    msaaQualityLevels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE;
+    msaaQualityLevels.NumQualityLevels = 0;
+
+    dx12Components.logicalDevice->CheckFeatureSupport(
+        D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS,
+        &msaaQualityLevels,
+        sizeof(msaaQualityLevels));
+
+    dx12FeaturesSupport.msaa4xQuality = msaaQualityLevels.NumQualityLevels;
+    assert(dx12FeaturesSupport.msaa4xQuality > 0);
+
+    // create command objects
+
+    // create swapchain
+
+    // create rtv and dsv descriptor heaps
 
     // main loop
     running = true;
